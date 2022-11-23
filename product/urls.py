b579a1fc94643b93cbc3a778d8e18c14f5c4ef02
bhargav.s <bhargavs.spaceo@gmail.com>
2022-11-23 15:23:20 +0530

    changes in urls

--- product/urls.py
+++ product/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('login', LoginAPIView.as_view(), name="login"),
     path('create-product', CreateProductAPIView.as_view(), name="create_product"),
     path('retrieve-product/', RetrieveProductAPIView.as_view(), name="retrieve_product"),
-    path('update-produc/<str:id>', UpdateProductAPIView.as_view(), name="update_product"),
+    path('update-product/<str:id>', UpdateProductAPIView.as_view(), name="update_product"),
     path('delete-product/<str:id>', DeleteProductAPIView.as_view(), name="delete_product"),
 ]
